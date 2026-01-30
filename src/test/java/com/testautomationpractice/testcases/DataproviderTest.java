8b9d18b309e9847b2ec01c30b5bc898f901a386a
Susmitha555 <sugutaru11@gmail.com>
2026-01-30 15:37:39 +0530

    removed unused imports

--- src/test/java/com/testautomationpractice/testcases/DataproviderTest.java
+++ src/test/java/com/testautomationpractice/testcases/DataproviderTest.java
@@ -1,14 +1,8 @@
 package com.testautomationpractice.testcases;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+
 import org.testng.annotations.Test;
 
 import com.testautomationpractice.base.TestBase;
